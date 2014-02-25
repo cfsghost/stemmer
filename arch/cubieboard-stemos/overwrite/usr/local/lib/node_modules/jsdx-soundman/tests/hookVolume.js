a448769772f160b7154a60034ca50de1295f4b22
Fred Chien <cfsghost@gmail.com>
2014-02-25 13:42:00 +0800

    support sound API

---
+++ arch/cubieboard-stemos/overwrite/usr/local/lib/node_modules/jsdx-soundman/tests/hookVolume.js
@@ -0,0 +1,15 @@
+
+var Soundman = require('../');
+
+var soundman = new Soundman;
+
+soundman.init(function(err) {
+	if (err) {
+		console.log(err);
+		return;
+	}
+
+	soundman.on('SinkNotify', function() {
+		console.log(soundman.getVolume());
+	});
+});
